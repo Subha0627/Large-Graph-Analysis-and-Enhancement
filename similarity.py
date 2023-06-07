2619b39c47f66373baee20969054f4b593f216f0
Subha0627 <subhamoym88@gmail.com>
2023-06-07 18:41:28 +0530

    June_7_18.41_Commit

--- similarity.py
+++ similarity.py
@@ -1,14 +1,9 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 import numpy as np
 import itertools
 """Finding Similarity between two graphs with respect to the betweenness centrality"""
 
-
-def compare_betweenness_centrality(original_graph, optimized_graph, removed_nodes):
-    """Setting the size to the size of the orginal graph"""
-    # The second graph is the reduced graph
-    reduced_graph = optimized_graph
+def compare_betweenness_centrality(original_graph, reduced_graph, removed_nodes):
     # Calculate betweenness centrality for the first graph
     betweenness_centrality_original_graph = nx.betweenness_centrality(original_graph)
     # Calculate betweenness centrality for the second graph
@@ -29,22 +24,22 @@ def compare_betweenness_centrality(original_graph, optimized_graph, removed_node
     return round(similarity_percentage, 2)
 
 
-"""Calculating Similarity between the original graph and the reduced graph with respect to the degree distribution"""
-
-
-def calculate_degree_sequence(original_graph, optimized_graph):
+"""
+Calculating Similarity between the original graph and the reduced graph with respect to the degree distribution
+"""
+def calculate_degree_sequence(original_graph, reduced_graph):
     # Find the degree sequence
     """Finding the degree sequence of the Original Graph"""
     degree_sequence_original = [degree for node, degree in original_graph.degree()]
     # Find the degree sequence
     """Finding the degree sequence of the Reduced Graph"""
-    degree_sequence_reduced = [degree for node, degree in optimized_graph.degree()]
+    degree_sequence_reduced = [degree for node, degree in reduced_graph.degree()]
     return degree_sequence_original, degree_sequence_reduced
 
 
-"""Calculate Degree Distribution from a given degree and number of nodes"""
-
-
+"""
+Calculate Degree Distribution from a given degree and number of nodes
+"""
 def calculate_degree_distribution(degree_sequence, size):
     # Initialize an array to store the degree distribution
     degree_distribution = np.zeros(size)
@@ -61,12 +56,12 @@ def calculate_degree_distribution(degree_sequence, size):
     return degree_distribution
 
 
-"""Two degree distributions 'array1' and 'array2' are compared and the similarity score is returned"""
-
-
-def compare_degree_distributions(original_graph, optimized_graph):
+"""
+Two degree distributions 'array1' and 'array2' are compared and the similarity score is returned
+"""
+def compare_degree_distributions(original_graph, reduced_graph):
     degree_sequence_original, degree_sequence_reduced = calculate_degree_sequence(
-        original_graph, optimized_graph)
+        original_graph, reduced_graph)
     size = len(original_graph)
     """Get the degree distribution for the degree sequence of the origianl graph"""
     degree_distribution_original = calculate_degree_distribution(degree_sequence_original, size)
@@ -103,19 +98,16 @@ If the reduction significantly impacts the interpretation or understanding of th
 alternative methods to retain more of the original graph's structure. Ultimately, whether the reduced graph is considered "bad" or 
 not depends on its utility and relevance to the particular analysis or problem at hand.
 """
-
-
-def compare_clustering_coefficient(original_graph, optimized_graph):
+def compare_clustering_coefficient(original_graph, reduced_graph):
     original_clustering_coefficient = nx.average_clustering(original_graph)
-    reduced_clustering_coefficient = nx.average_clustering(optimized_graph)
-    similarity_clustering_coefficient = reduced_clustering_coefficient / original_clustering_coefficient
+    reduced_clustering_coefficient = nx.average_clustering(reduced_graph)
+    similarity_clustering_coefficient = 1-abs(original_clustering_coefficient-reduced_clustering_coefficient)/max(original_clustering_coefficient, reduced_clustering_coefficient)
     return round(similarity_clustering_coefficient*100, 2)
 
-
 # Calculate eigenvector centralities
-def compare_eigen_vector_centrality(original_graph, optimized_graph):
+def compare_eigen_vector_centrality(original_graph, reduced_graph):
     eigenvector_centrality_original = nx.eigenvector_centrality(original_graph)
-    eigenvector_centrality_reduced = nx.eigenvector_centrality(optimized_graph)
+    eigenvector_centrality_reduced = nx.eigenvector_centrality(reduced_graph)
     # Get the common nodes present in both graphs
     common_nodes = set(eigenvector_centrality_original.keys()).intersection(
         eigenvector_centrality_reduced.keys())
@@ -131,7 +123,6 @@ def compare_eigen_vector_centrality(original_graph, optimized_graph):
     # Convert the MSE to a percentage scale
     similarity_percentage = 100 * (1 - mse)
     return round(similarity_percentage, 2)
-
 
 def average_path_length(graph):
     total_length = 0
@@ -150,16 +141,14 @@ def average_path_length(graph):
     else:
         return 0
 
-
-def compare_average_path_length(original_graph, optimized_graph):
+def compare_average_path_length(original_graph, reduced_graph):
     # Calculate the average path length
     average_path_length_original = average_path_length(original_graph)
-    average_path_length_reduced = average_path_length(optimized_graph)
+    average_path_length_reduced = average_path_length(reduced_graph)
 
     # Print the average path length
     similarity_avg_path_length = round(1-abs(average_path_length_reduced-average_path_length_original)/max(average_path_length_original, average_path_length_reduced), 2)*100
     return similarity_avg_path_length
-
 
 """
 The formula `(graph1.number_of_nodes() * (graph1.number_of_nodes() - 1) / 2)` calculates the total number of possible pairs of nodes in `graph1`
@@ -171,8 +160,6 @@ by this total number of possible pairs, and then multiply by 100 to express the 
 By using this formula, we can obtain a normalized measure of similarity between the graphs that accounts for the total possible pairs 
 of nodes in `graph1`.
 """
-
-
 def calculate_jaccard_similarity(graph1, graph2):
     jaccard_scores = []
     for u, v in itertools.combinations(graph1.nodes(), 2):
